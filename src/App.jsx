5d20018a18abcebe5791dbbfa23e9029bc8ed80d
Miguel Ortiz <miguelortiz.code@gmail.com>
2026-05-14 17:29:29 -0500

    feat: create Sidebar component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { Header } from "./components/layout/Header";
-
+import { Sidebar } from "./components/layout/Sidebar";
 function App() {
 
 
   return (
     <>
       <Header />
+      <div class="grid contenedor contenido-principal">
+        <Sidebar />
+
+        <main class="caja-contenido col-9">
+           // TODO:  
+        </main>
+      </div>
     </>
   )
 }
---
+++ src/components/layout/Sidebar.jsx
@@ -0,0 +1,11 @@
+export const Sidebar  =  () => (
+    <aside className="sidebar col-3">
+        <h2>Administración</h2>
+        <nav className="navegacion">
+            <a href="index.html" className="clientes">Clientes</a>
+            <a href="productos.html" className="productos">Productos</a>
+            <a href="pedidos.html" className="pedidos">Pedidos</a>
+        </nav>
+    </aside>
+
+);
