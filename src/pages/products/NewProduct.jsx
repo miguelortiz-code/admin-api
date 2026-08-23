04deb1e01066d74305c66d62405e03834a79937f
Miguel Ortiz <miguelortiz.code@gmail.com>
2026-08-22 20:39:20 -0500

    feat: add product creation form.

--- src/pages/products/NewProduct.jsx
+++ src/pages/products/NewProduct.jsx
@@ -1,5 +1,42 @@
+import { useState } from "react";
+
 export const NewProduct = () =>{
+    
+    // producto = state, setState = guardarProducto
+    const [product, setProducts] = useState({
+        name: '',
+        price: ''
+    });
+    // archivo/ file = state, setFile = Guardar imagen
+    const [image, setImage] = useState('');
+    
     return(
-        <h1>Nuevo Producto</h1>
+        <>
+            <h2>Nuevo Producto</h2>
+
+            <form>
+                <legend>Llena todos los campos</legend>
+
+                <div className="campo">
+                    <label>Nombre:</label>
+                    <input type="text" placeholder="Nombre Producto" name="nombre" />
+                </div>
+
+                <div className="campo">
+                    <label>Precio:</label>
+                    <input type="number" name="precio" min="0.00" step="0.01" placeholder="Precio" />
+                </div>
+            
+                <div className="campo">
+                    <label>Imagen:</label>
+                    <input type="file"  name="imagen" />
+                </div>
+
+                <div className="enviar">
+                    <input type="submit" className="btn btn-azul" value="Agregar Producto" />
+                </div>
+            </form>
+        
+        </>
     )
 };
